e2c662589f5c9002fb30e3d24360c9956050f102
Siwar Ghozzi <ghozzisiwar90@gmail.com>
2021-08-02 15:00:43 +0100

    change by siwar

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Siwar!"); 
+        System.out.println("Hello, Second Test!"); 
     }
 }
 // hello sondes
